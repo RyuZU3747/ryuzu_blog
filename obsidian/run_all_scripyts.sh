fc0897833ff2868dc8bf9916db1e193f6fe7e7a2
RyuZU3747 <dbrua1222@naver.com>
2025-05-07 23:18:26 +0900

    update blog

--- obsidian/run_all_scripyts.sh
+++ obsidian/run_all_scripyts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-SYNC_POSTS=C:/git/ryuzu_blog/obsidian/sync_posts.py
-HANDLE_IMGES=C:/git/ryuzu_blog/obsidian/handle_images.py
-YOUTUBE_PATH=C:/git/ryuzu_blog/obsidian/update_youtube_path.py
+
+cd C:/git/ryuzu_blog/
+
+SYNC_POSTS=./obsidian/sync_posts.py
+HANDLE_IMGES=./obsidian/handle_images.py
+YOUTUBE_PATH=./obsidian/update_youtube_path.py
 
 python $SYNC_POSTS
 python $HANDLE_IMGES
